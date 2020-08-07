6036501ddd8cf6ec3a27f8474738534658e72016
Misaki Ata <misakiata209625@gamil.com>
2020-08-07 20:43:56 +0900

    更新！

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ def template():
     py_name = "あたあああああ"
     return render_template("index.html", name = py_name)
 
+@app.route("/anime")
+def anime():
+    return render_template("anime.html") 
+
+@app.route("/weather")
+def weather():
+    py_weather = "晴れ"
+    return render_template("weather.html" , name = py_weather)
+    
 
 
 
