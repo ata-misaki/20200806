3dc40d6a1106a1603e20e88f0fae607c813dc15d
Misaki Ata <misakiata209625@gamil.com>
2020-08-10 19:09:55 +0900

    課題回答

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Flaskからインポートしてflaskを使えるようにする
 from flask import Flask, render_template
+import sqlite3
 
 # appという名前でFlaskあぷりをつくっていくよ
 app = Flask(__name__)
@@ -21,11 +22,33 @@ def template():
 def anime():
     return render_template("anime.html") 
 
+
+
 @app.route("/weather")
 def weather():
     py_weather = "晴れ"
     return render_template("weather.html" , name = py_weather)
     
+@app.route("/dbtest")
+def dbtest():
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    # 課題１ SQL文を書いてスタッフ1名の情報を取得
+    c.execute("SELECT id,name,age,adress FROM staff WHERE id = 1" )
+
+    staff_info = c.fetchone()
+    c.close()
+    print(staff_info[0])
+
+
+
+    #課題２ 新たなHTMLを作成し、１で取得したスタッフの情報をそれぞれ表示する
+
+    # return render_template("index.html" ,id = staff_info[0] ,name = staff_info[1] ,age = staff_info[2] , adress = staff_info[3])
+    return render_template("index.html" ,list = staff_info)
+
+if __name__ == "__main__":
+    app.run(debug = True)
 
 
 
