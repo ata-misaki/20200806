42cdac2ca071a9f38bbaf8467d2a5f7fe47cc018
Misaki Ata <misakiata209625@gamil.com>
2020-08-18 20:03:22 +0900

    卒業制作

--- app.py
+++ app.py
@@ -11,43 +11,6 @@ app.secret_key = 'sunabaco_yatsusiro'
 def helloWorld():
     return "Hello World."
 
-@app.route("/<name>")
-def greet(name):
-    return name + "さん、はろー！"
-
-@app.route("/template")
-def template():
-    py_name = "あたあああああ"
-    return render_template("index.html", name = py_name)
-
-@app.route("/anime")
-def anime():
-    return render_template("anime.html") 
-
-
-
-@app.route("/weather")
-def weather():
-    py_weather = "晴れ"
-    return render_template("weather.html" , name = py_weather)
-    
-@app.route("/dbtest")
-def dbtest():
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
-    # 課題１ SQL文を書いてスタッフ1名の情報を取得
-    c.execute("SELECT id,name,age,adress FROM staff WHERE id = 1" )
-
-    staff_info = c.fetchone()
-    c.close()
-    print(staff_info[0])
-
-
-
-    #課題２ 新たなHTMLを作成し、１で取得したスタッフの情報をそれぞれ表示する
-
-    # return render_template("index.html" ,id = staff_info[0] ,name = staff_info[1] ,age = staff_info[2] , adress = staff_info[3])
-    return render_template("index.html" ,list = staff_info)
 
 @app.route("/list")
 def tasklist():
